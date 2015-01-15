c62165ec58e3a405303ebb00f97ab4e4584b7a67
Hamad Nji <Hamad Nji@Hamad_007.connectify>
2015-01-15 08:58:05 +0100

    hghg

--- Lecteur Audio/src/view/Appli_Lecteur.java
+++ Lecteur Audio/src/view/Appli_Lecteur.java
@@ -13,6 +13,7 @@ import javax.swing.JSeparator;
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
+import javax.swing.JProgressBar;
 
 public class Appli_Lecteur extends JFrame {
 
@@ -40,82 +41,115 @@ public class Appli_Lecteur extends JFrame {
 	public Appli_Lecteur() {
 		setTitle("Mini Lecteur");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 815, 430);
+		setBounds(100, 100, 815, 450);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnMedia = new JMenu("Media");
-		menuBar.add(mnMedia);
+		JMenu media = new JMenu("M\u00E9dia");
+		menuBar.add(media);
 		
-		JMenuItem mntmOuvrirUnMdia = new JMenuItem("Ouvrir un m\u00E9dia");
-		mntmOuvrirUnMdia.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
-		mnMedia.add(mntmOuvrirUnMdia);
+		JMenuItem ouv_med = new JMenuItem("Ouvrir un m\u00E9dia");
+		ouv_med.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		media.add(ouv_med);
 		
-		JMenuItem mntmOuvrirUnDossier = new JMenuItem("Ouvrir un dossier");
-		mntmOuvrirUnDossier.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_MASK));
-		mnMedia.add(mntmOuvrirUnDossier);
+		JMenuItem ouv_dos = new JMenuItem("Ouvrir un dossier");
+		ouv_dos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_MASK));
+		media.add(ouv_dos);
 		
-		JMenuItem mntmOuvrirUnDisque = new JMenuItem("Ouvrir un disque");
-		mntmOuvrirUnDisque.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));
-		mnMedia.add(mntmOuvrirUnDisque);
+		JMenuItem ouv_dis = new JMenuItem("Ouvrir un disque");
+		ouv_dis.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));
+		media.add(ouv_dis);
 		
 		JSeparator separator = new JSeparator();
-		mnMedia.add(separator);
+		media.add(separator);
 		
-		JMenuItem mntmEnregistrerLaListe = new JMenuItem("Enregistrer la liste de lecture");
-		mntmEnregistrerLaListe.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
-		mnMedia.add(mntmEnregistrerLaListe);
+		JMenuItem enr_list = new JMenuItem("Enregistrer la liste de lecture");
+		enr_list.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		media.add(enr_list);
 		
 		JSeparator separator_1 = new JSeparator();
-		mnMedia.add(separator_1);
+		media.add(separator_1);
 		
 		JMenu mnMdiaRcents = new JMenu("M\u00E9dia r\u00E9cents");
-		mnMedia.add(mnMdiaRcents);
+		media.add(mnMdiaRcents);
 		
-		JMenuItem mntmQuitter = new JMenuItem("Quitter");
-		mntmQuitter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, InputEvent.ALT_MASK));
-		mnMedia.add(mntmQuitter);
+		JMenuItem quitter = new JMenuItem("Quitter");
+		quitter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, InputEvent.ALT_MASK));
+		media.add(quitter);
 		
-		JMenu mnLecture = new JMenu("Lecture");
-		menuBar.add(mnLecture);
+		JMenu lecture = new JMenu("Lecture");
+		menuBar.add(lecture);
 		
-		JMenuItem mntmPlay = new JMenuItem("Jouer");
-		mnLecture.add(mntmPlay);
+		JMenuItem jouer = new JMenuItem("Jouer");
+		lecture.add(jouer);
 		
-		JMenuItem mntmStop = new JMenuItem("Stop");
-		mnLecture.add(mntmStop);
+		JMenuItem stop = new JMenuItem("Stop");
+		lecture.add(stop);
 		
-		JMenuItem mntmPrcedent = new JMenuItem("Pr\u00E9cedent");
-		mnLecture.add(mntmPrcedent);
+		JMenuItem prec = new JMenuItem("Pr\u00E9cedent");
+		lecture.add(prec);
 		
-		JMenuItem mntmSuivant = new JMenuItem("Suivant");
-		mnLecture.add(mntmSuivant);
+		JMenuItem suiv = new JMenuItem("Suivant");
+		lecture.add(suiv);
 		
-		JMenu mnAudio = new JMenu("Audio");
-		menuBar.add(mnAudio);
+		JMenu audio = new JMenu("Audio");
+		menuBar.add(audio);
 		
-		JMenuItem mntmAugmenterLaTonalit = new JMenuItem("Augmenter la tonalit\u00E9");
-		mnAudio.add(mntmAugmenterLaTonalit);
+		JMenuItem aug_ton = new JMenuItem("Augmenter la tonalit\u00E9");
+		audio.add(aug_ton);
 		
-		JMenuItem mntmRduireLaTonalit = new JMenuItem("R\u00E9duire la tonalit\u00E9");
-		mnAudio.add(mntmRduireLaTonalit);
+		JMenuItem red_ton = new JMenuItem("R\u00E9duire la tonalit\u00E9");
+		audio.add(red_ton);
 		
-		JMenuItem mntmCouperLeSon = new JMenuItem("Couper le son");
-		mnAudio.add(mntmCouperLeSon);
+		JMenuItem coup_son = new JMenuItem("Couper le son");
+		audio.add(coup_son);
 		
-		JMenu mnOutils = new JMenu("Outils");
-		menuBar.add(mnOutils);
+		JMenu outils = new JMenu("Outils");
+		menuBar.add(outils);
 		
-		JMenu mnVue = new JMenu("Vue");
-		menuBar.add(mnVue);
+		JMenu vue = new JMenu("Vue");
+		menuBar.add(vue);
 		
-		JMenu mnAide = new JMenu("Aide");
-		menuBar.add(mnAide);
+		JMenuItem list_lect = new JMenuItem("Liste de lecture");
+		vue.add(list_lect);
+		
+		JSeparator separator_2 = new JSeparator();
+		vue.add(separator_2);
+		
+		JMenuItem interf_min = new JMenuItem("Interface Minimale");
+		vue.add(interf_min);
+		
+		JMenuItem plein_ecran = new JMenuItem("Plein \u00E9cran");
+		vue.add(plein_ecran);
+		
+		JMenu aide = new JMenu("Aide");
+		menuBar.add(aide);
+		
+		JMenuItem aide_f = new JMenuItem("Aide");
+		aide_f.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+		aide.add(aide_f);
+		
+		JSeparator separator_3 = new JSeparator();
+		aide.add(separator_3);
+		
+		JMenuItem a_propos = new JMenuItem("A propos");
+		a_propos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, InputEvent.SHIFT_MASK));
+		aide.add(a_propos);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 799, 300);
+		contentPane.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 311, 799, 68);
+		contentPane.add(panel_1);
+		
+		JProgressBar progressBar = new JProgressBar();
+		panel_1.add(progressBar);
 	}
-
 }
